1550a3b4721b8b15e6c22d92f663170b1bfc67ab
HJKL <bbw1996@qq.com>
2023-11-12 17:24:13 +0800

    feat: 性能监控

--- src/monitor/index.js
+++ src/monitor/index.js
@@ -1,7 +1,9 @@
 import { blankScreen } from "./lib/blankScreen";
 import { injectJsError } from "./lib/jsError";
+import { timing } from "./lib/timing";
 import injectXHR from "./lib/xhr";
 
 injectJsError();
 injectXHR();
 blankScreen();
+timing();
